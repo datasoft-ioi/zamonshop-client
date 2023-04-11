152292a0f46a7676bbcfb97ccd0ba53ada4dca65
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-04-11 14:46:07 +0500

    respansive bottom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,75 +3,97 @@ import './footer.css'
 
 function Footer() {
 
-  const [uzPay , setUzPay] = useState([
+  const [uzPay, setUzPay] = useState([
     {
-      id: 1 ,
+      id: 1,
       image: 'https://logobank.uz:8005/media/logos_png/Click-01.png'
     },
     {
-      id: 2 ,
+      id: 2,
       image: 'https://logobank.uz:8005/media/logos_png/Apelsin-01.png'
     },
     {
-      id: 3 ,
+      id: 3,
       image: 'https://logobank.uz:8005/media/logos_png/Uzcard-01.png'
     },
     {
-      id: 4 ,
+      id: 4,
       image: 'https://logobank.uz:8005/media/logos_png/payme-01.png'
     }
   ])
 
   return (
-    <div className="Footer">
-      <footer class="footer">
-        <div class="container">
-          <div class="row">
-            <div class="footer-col">
-              <h4>Bog'lanish</h4>
-              <ul className='paddingzero'>
-                <li className='phonenumber default-li'><i class="fa-solid fa-phone"></i><h2>+998943614656</h2></li>
-                <li className='tg-silka default-li'><i class="fab fa-telegram"></i><a href="https://t.me/AA_6464">Asadbek Abdullayev</a></li>
-              </ul>
-            </div>
-            <div class="footer-col">
-              <h4>Kompaniya</h4>
-              <ul>
-                <li><a href="#">Biz haqimizda</a></li>
-                <li><a href="#">Bizning hizmatlar</a></li>
-                <li><a href="#">Xavfsizlik</a></li>
-              </ul>
-            </div>
-            <div class="footer-col">
-              <h4>Yordam 24/7</h4>
-              <ul>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Telegram yordam</a></li>
-                <li><a href="#">returns</a></li>
-                <li><a href="#">+998943614656</a></li>
-                <li><a href="#">To'lov usullari</a></li>
-              </ul>
-            </div>
-            <div class="footer-col">
-              <h4>Ijtimoiy tarmoqlar</h4>
-              <div class="social-links">
-                <a href="#"><i class="fab fa-telegram"></i></a>
-                <a href="#"><i class="fab fa-youtube"></i></a>
-                <a href="https://instagram.com/zamonshop"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+    <>
+      <div className="mobileFooter">
+        <ul>
+          <li>
+          <img src="https://img.icons8.com/ios/50/null/home--v1.png"/>
+            <span>Home</span>
+          </li>
+          <li>
+            <img src="https://img.icons8.com/ios-glyphs/30/null/hearts.png" />
+            <span>Izberenya</span>
+          </li>
+          <li>
+            <img src="https://img.icons8.com/ios/50/null/shopping-bag--v1.png" />
+            <span>Korzinka</span>
+          </li>
+          <li>
+            <img src="https://img.icons8.com/ios/50/null/gender-neutral-user--v1.png" />
+            <span>Profil</span>
+          </li>
+        </ul>
+      </div>
+      <div className="Footer">
+        <footer class="footer">
+          <div class="container">
+            <div class="row">
+              <div class="footer-col">
+                <h4>Bog'lanish</h4>
+                <ul className='paddingzero'>
+                  <li className='phonenumber default-li'><i class="fa-solid fa-phone"></i><h2>+998943614656</h2></li>
+                  <li className='tg-silka default-li'><i class="fab fa-telegram"></i><a href="https://t.me/AA_6464">Asadbek Abdullayev</a></li>
+                </ul>
               </div>
-              <div className="uzPay">
-                {
-                  uzPay.map(item => (
-                    <a href=""><button><img src={item.image} alt="" /></button></a>
-                  ))
-                }
+              <div class="footer-col">
+                <h4>Kompaniya</h4>
+                <ul>
+                  <li><a href="#">Biz haqimizda</a></li>
+                  <li><a href="#">Bizning hizmatlar</a></li>
+                  <li><a href="#">Xavfsizlik</a></li>
+                </ul>
+              </div>
+              <div class="footer-col">
+                <h4>Yordam 24/7</h4>
+                <ul>
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">Telegram yordam</a></li>
+                  <li><a href="#">returns</a></li>
+                  <li><a href="#">+998943614656</a></li>
+                  <li><a href="#">To'lov usullari</a></li>
+                </ul>
+              </div>
+              <div class="footer-col">
+                <h4>Ijtimoiy tarmoqlar</h4>
+                <div class="social-links">
+                  <a href="#"><i class="fab fa-telegram"></i></a>
+                  <a href="#"><i class="fab fa-youtube"></i></a>
+                  <a href="https://instagram.com/zamonshop"><i class="fab fa-instagram"></i></a>
+                  <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div className="uzPay">
+                  {
+                    uzPay.map(item => (
+                      <a href=""><button><img src={item.image} alt="" /></button></a>
+                    ))
+                  }
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </>
   );
 }
 
